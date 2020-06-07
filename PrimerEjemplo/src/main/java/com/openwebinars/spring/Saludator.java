6230664a80087e5e97cee514d474fdaee4ab66f8
David <doubled000000@gmail.com>
2020-06-07 18:08:19 +0100

    Ejemplo bean constructor

--- PrimerEjemplo/src/main/java/com/openwebinars/spring/Saludator.java
+++ PrimerEjemplo/src/main/java/com/openwebinars/spring/Saludator.java
@@ -3,7 +3,9 @@ package com.openwebinars.spring;
 public class Saludator {
 	private String mensaje;
 	
-
+	public Saludator(String str) {
+		this.mensaje = str;
+	}
 	public String saludo(){
 		return (mensaje == null) ? "Hola Mundo!!!" : mensaje;
 	}
